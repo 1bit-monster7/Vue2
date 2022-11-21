b5fb3ad1b8273d28997055e8af7485387fc5cf2f
张文彬 <1748244285@qq.com>
2022-11-21 10:03:03 +0800

    1.中文

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
+  lintOnSave: false, //禁用eslit
   productionSourceMap: false,
   devServer: {
     port: port,
